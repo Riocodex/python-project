2498e19971f2f6d5987b1c70c9742dc9eec2dcd0
Riocodex <79114676+Riocodex@users.noreply.github.com>
2021-07-07 13:44:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+#1)name=input("enter name please")
+# if name== 'Alice':
+#     print("Hi Alice")
+
+#2)
+# name=input("enter name please")
+# if name== 'Alice':
+#     print("Hi Alice")
+# else:
+#     print("hello stranger")
+
+#3)
+# name=input("enter name please")
+# if name== 'Alice':
+#     print("Hi Alice")
+# else:
+#     age=int(input("enter age please"))
+#     if age<12:
+#         print("you are not alice kiddo")
+
+# 4)
+# name=input("enter name please")
+# if name== 'Alice':
+#     print("Hi Alice")
+# else:
+#     age=int(input("enter age please"))
+#     if age<12:
+#         print("you are not alice kiddo")
+#     else:
+#         age = int(input("enter age please"))
+#         if age > 2000:
+#             print("Unlike u alice is not an undead immortal vampire")
+#         else:
+#             age = int(input("enter age please"))
+#             if age >100:
+#                 print("you are not Alice granny")
+
+
+#5)
+# name=input("enter name please")
+# if name== 'Alice':
+#     print("Hi Alice")
+# else:
+#     age=int(input("enter age please"))
+#     if age<12:
+#         print("you are not alice kiddo")
+#     else:
+#         age = int(input("enter age please"))
+#         if age > 100:
+#             print("you are not Alice granny")
+#         else:
+#             age = int(input("enter age please"))
+#             if age > 2000:
+#                 print("Unlike u alice is not an undead immortal vampire")
+
+
+#6)
+# name=input("enter name please")
+# # if name== 'Alice':
+# #     print("Hi Alice")
+# # else:
+# #     age=int(input("enter age please"))
+# #     if age<12:
+# #         print("you are not alice kiddo")
+# #     else:
+# #         print("You are neither alice nor a kid!")
+
+#6
+# rainAns=input("is it raining?(input yes or no)")
+# if rainAns=='yes':
+#     umbrella=input("Do u have an umbrella(input yes or no)")
+#     if umbrella=='yes':
+#         print("go outside")
+#     else:
+#         while umbrella=='no':
+#             print("wait a while")
+#             rainAns2=input("is it raining?(input yes or no)")
+#             if rainAns2=='no':
+#                 print("go outside")
+#                 break
+# else:
+#     print("go outside")
+
+#7)
+# spam=int(input("input a number"))
+# if spam<5:
+#     print("hello world")
+#     spam+=1
+
+#8)
+spam=int(input("input a number"))
+while spam<5:
+    print("hello world")
+    spam+=1
+
+
+
+
+
+
+
+
